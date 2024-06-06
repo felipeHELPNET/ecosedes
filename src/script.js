7bad048a561c3f080137879d5e0d22ff80fb388c
felipeHELPNET <felipehelpnet@gmail.com>
2024-06-06 13:50:58 -0300

    up1-Modal

--- src/script.js
+++ src/script.js
@@ -7,13 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     slides.style.transform = `translateX(${-slideWidth * index}px)`;
   }
 
-  document.querySelector(".btn-prev").addEventListener("click", () => {
+  document.querySelector("#btn-prev").addEventListener("click", () => {
     slideIndex--;
     if (slideIndex < 0) slideIndex = 0;
     showSlide(slideIndex);
   });
 
-  document.querySelector(".btn-next").addEventListener("click", () => {
+  document.querySelector("#btn-next").addEventListener("click", () => {
     slideIndex++;
     if (slideIndex >= document.querySelectorAll(".slides img").length)
       slideIndex = 0;
@@ -23,3 +23,23 @@ document.addEventListener("DOMContentLoaded", function () {
   // Mostrar o primeiro slide inicialmente
   showSlide(slideIndex);
 });
+
+// MODAL =
+
+document.getElementById("abrirModal").addEventListener("click", function () {
+  document.getElementById("modal").style.display = "block";
+});
+
+// Quando o usuário clica fora do modal ou no botão fechar
+window.onclick = function (event) {
+  if (event.target == document.getElementById("modal")) {
+    document.getElementById("modal").style.display = "none";
+  }
+};
+
+// Função para fechar o modal quando o botão fechar é clicado
+document.querySelector(".fechar").onclick = function () {
+  document.getElementById("modal").style.display = "none";
+};
+
+// Fim Modal
